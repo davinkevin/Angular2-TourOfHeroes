4eb5287dae475f8b9415412c6e157138615e158c
Davin Kevin <davin.kevin@gmail.com>
2016-03-05 20:19:03 +0100

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by kevin on 05/03/2016.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export default class AppComponent {}
+
