33fb4df53751a3005c07ae250cfee2a29de26d56
Davin Kevin <davin.kevin@gmail.com>
2016-03-05 20:30:43 +0100

    feat(editing): live editing of the character

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,28 @@
  */
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+          <h1>{{title}}</h1>
+          <h2>{{hero.name}} details!</h2>
+          <div><label>id: </label>{{hero.id}}</div>
+          <div>
+            <label>name: </label>
+            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+          </div>
+  `
 })
-export default class AppComponent {}
+export default class AppComponent {
+    public title = 'Star Wars Characters';
+    public hero : Hero = {
+        id : 1,
+        name : 'Luke Skywalker'
+    };
+}
 
