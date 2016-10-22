31df799364adf5111d74fde522825f0eb43ba4dc
davinkevin <davin.kevin@gmail.com>
2016-10-22 20:20:07 +0200

    feat(): reset master to re-start the whole tutorial

--- app/main.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kevin on 05/03/2016.
- */
-import {bootstrap}    from 'angular2/platform/browser';
-import {Type} from "angular2/core";
-import {AppComponent} from './app.component';
-
-bootstrap(<Type>AppComponent);
