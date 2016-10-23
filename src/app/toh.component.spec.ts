015b042c74bd7a5c308670d72dd46b2eb9402bab
davinkevin <davin.kevin@gmail.com>
2016-10-23 14:19:39 +0200

    feat(master/detail): add master/detail page to the TourOfHeroes
    Integration with test of Master/detail detailed at https://angular.io/docs/ts/latest/tutorial/toh-pt2.html.

--- src/app/toh.component.spec.ts
+++ src/app/toh.component.spec.ts
@@ -8,8 +8,22 @@ import {BrowserDomAdapter} from '@angular/platform-browser/src/browser/browser_a
 describe('App: Angular2TourOfHeroes', () => {
 
   let fixture: ComponentFixture<TohComponent>,
-      compiled: Element,
-      app: TohComponent;
+    compiled: HTMLElement,
+    app: TohComponent;
+
+  let heroes = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magneta'},
+    {id: 16, name: 'RubberMan'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Dr IQ'},
+    {id: 19, name: 'Magma'},
+    {id: 20, name: 'Tornado'}
+  ];
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -32,30 +46,50 @@ describe('App: Angular2TourOfHeroes', () => {
     expect(app.title).toEqual('Tour of Heroes');
   }));
 
+  it('should have list of heroes attached', async(() => {
+    expect(app.heroes).toEqual(heroes);
+  }));
+
+  it('should have no selected heroes defiened', async(() => {
+    expect(app.selectedHero).toBeUndefined();
+  }));
+
   it('should have title in a h1 tag', async(() => {
     /* Given */
     /* When  */
     fixture.detectChanges();
     /* Then  */
-    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
+    expect(compiled.querySelector('h2').textContent).toContain('My Heroes');
   }));
 
-  it('should have sub-title associated to hero.name', async(() => {
+  it('should have the list of heroes shown', async(() => {
     /* Given */
+    let actual = heroes.map(h => h.name);
+
     /* When  */
     fixture.detectChanges();
+    let lis = [].slice.call(compiled.querySelectorAll('ul.heroes li'))
+      .map(li => li.childNodes[2].textContent.trim());
+
     /* Then  */
-    expect(compiled.querySelector('h2').textContent).toContain('Windstorm details!');
+    expect(lis.length).toEqual(10);
+    expect(lis).toEqual(actual);
   }));
 
-  it('should have two-way binding on field and h2', () => {
+  it('should show edition if element is selected', async(() => {
     /* Given */
     fixture.detectChanges();
+    let selectedElement = (<HTMLElement> compiled.querySelector('li:first-child'));
+
     /* When  */
-    app.hero.name = 'Foo';
+    selectedElement.click();
     fixture.detectChanges();
+
     /* Then  */
-    expect(compiled.querySelector('h2').textContent).toContain('Foo details!');
-  });
+    expect(app.selectedHero).toBeDefined();
+    expect(app.selectedHero).toEqual(heroes[0]);
+    expect(compiled.querySelectorAll('h2')[1].textContent).toEqual(`${app.selectedHero.name} details!`);
+    expect(selectedElement.className).toEqual('selected');
+  }));
 
 });
